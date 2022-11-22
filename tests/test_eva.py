c46a6f979e5e25871e22f8a5df72fdcc6c452372
Jake Bailey <asaxplayinghorse@gmail.com>
2022-11-22 15:52:33 -0800

    fix: test bug. tests fully refactored

--- tests/test_eva.py
+++ tests/test_eva.py
@@ -33,66 +33,54 @@ def test_var():
 def test_varAccess():
     testUtils.test(eva,
     """
-        (begin (var 'x' 10)
-        ('x'))
+        (begin (var x 10)
+        x)
     """, 10)
-    #eva.eval(['var', 'x', 10])
-    #assert eva.eval(['x']) == 10
 
 def test_block():
-    assert eva.eval(
-        ['begin',
-            ['var', 'x', 10],
-            ['var', 'y', 20],
-            ['+', ['*', 'x', 'y'], 30],
-        ]) == 230
-
-def test_assignment():
-    assert eva.eval(
-        ['begin',
-            ['var', 'data', 10],
-            ['begin',
-                ['set', 'data', 100]
-            ],
-            'data'
-        ]
-    ) == 100
-
-def test_if():
-    assert eva.eval(
-        ['begin',
-            ['var', 'x', 10],
-            ['var', 'y', 0],
-
-            ['if', ['>', 'x', 10],
-                ['set', 'y', 20],
-                ['set', 'y', 30]
-            ],
-            'y'
-        ]
-    ) == 30
-
-def test_while():
-    assert eva.eval(
-        ['begin',
-            ['var', 'counter', 0],
-            ['var', 'result', 0],
-            ['while', ['<', 'counter', 10],
-                # result++
-                # TODO: implement ['++', <Exp>]
-                ['begin',
-                    ['set', 'result', ['+', 'result', 1]],
-                    ['set', 'counter', ['+', 'counter', 1]]
-                ]
-            ],
-            'result'
-        ]
-    )
-
-def test_testUtils():
-    testUtils.test(eva, """
+    testUtils.test(eva,
+    """
         (begin
             (var x 10)
             (var y 20)
-            (+ (* x 10) y))
-    """, 120)
+            (+ (* x y) 30)
+        )
+    """, 230)
+
+def test_varAssignment():
+    testUtils.test(eva,
+    """
+        (begin
+            (var data 10)
+            (begin
+                (set data 100)
+            )
+            data
+        )
+    """, 100)
+
+def test_if():
+    testUtils.test(eva,
+    """
+        (begin
+            (var x 10)
+            (var y 0)
+            (if (> x 10)
+            (set y 20)
+            (set y 30))
+            y
+        )
+    """, 30)
+
+def test_while():
+    testUtils.test(eva,
+    """
+        (begin
+            (var counter 0)
+            (var result 0)
+            (while (< counter 10)
+            (begin
+                (set result (+ result 1))
+                (set counter (+ counter 1))))
+            result)
+    """, 10)
